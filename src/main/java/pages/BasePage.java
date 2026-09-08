ad5ad955dbb1340b2f0ac4e65b7c50f09cac583d
Tien <ttienkerbell@MacBookAir.lan>
2026-09-08 19:58:26 +0700

    Add BasePage

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,51 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected WebElement waitVisible (By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitClickable (By locator){
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void click (By locator) {
+        waitClickable(locator).click();
+    }
+
+    protected void type(By locator, String text){
+        WebElement el = waitVisible(locator);
+        el.clear();
+        el.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitVisible(locator).getText().trim();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        try {
+            return driver.findElement(locator).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+}
